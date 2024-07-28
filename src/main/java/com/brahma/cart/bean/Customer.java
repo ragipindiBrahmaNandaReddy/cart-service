dc757898d8cf7a80e4ac3fc0a5d74b5ff0c69f3b
brahma <brahma45678@gmail.com>
2024-07-28 16:47:29 +0530

    fix: added passwords filed to the customer

--- src/main/java/com/brahma/cart/bean/Customer.java
+++ src/main/java/com/brahma/cart/bean/Customer.java
@@ -19,6 +19,7 @@ public class Customer {
     private String name;
     private String email;
     private long phoneNumber;
+    private String password;
 
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Address> address;
